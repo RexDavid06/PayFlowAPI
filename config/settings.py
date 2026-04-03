9ad05498c07230d3a8b4a1189890935b95841d34
Buchi Rex-David <rhexmilia06@gmail.com>
2026-04-03 15:31:53 +0100

    added a comment in the settings.py

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_yasg', # for swagger documentation
-    'corsheaders',
+    'corsheaders', # for interacting with a frontend
 
     'accounts',
     'wallets.apps.WalletsConfig',
